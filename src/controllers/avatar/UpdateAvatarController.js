126af9e8cfe5feb167fb9346710aba262d60adcd
GeovaneMT <gmtgeovane@gmail.com>
2024-05-03 13:03:04 -0300

    fixes

--- src/controllers/avatar/UpdateAvatarController.js
+++ src/controllers/avatar/UpdateAvatarController.js
@@ -1,4 +1,4 @@
-const AppError = require("../../utils/AppError")
+const { AppError } = require("../../utils/AppError")
 const knex = require("../../dataBase/knex")
 const DiskStorage = require("../../providers/DiskStorage")
 
--- src/controllers/foodImage/UpdateFoodImage.js
+++ src/controllers/foodImage/UpdateFoodImage.js
@@ -1,48 +1,58 @@
-const AppError = require("../../utils/AppError")
-const knex = require("../../dataBase/knex")
-const DiskStorage = require("../../providers/DiskStorage")
+const { AppError } = require('../../utils/AppError')
+const knex = require('../../dataBase/knex')
+const DiskStorage = require('../../providers/DiskStorage')
 
-async function UpdateFoodImage(request, response) {
-  console.log("Image upload initiated...")
+async function UpdateImageController(request, response) {
+  console.log('Image upload initiated...')
 
-  const { id } = request.params;
-  const user_id = request.user.id;
-  constimageFilename = request.file.filename
+  const foodId = request.food.id
+  const imageFilename = request.file.filename
 
   const diskStorage = new DiskStorage()
 
   try {
     // Fetching food from the database
-    const food = await knex("foods").where({ id, user_id }).first()
-    console.log("Trying to fetch food from the database...")
-    console.log("Fetching image...")
+    const food = await knex('foods').where({ id: foodId }).first()
+    console.log('Trying to fetch food from the database...')
 
-    // Handling case where image already exists
+    // Handling case where user doesn't exist
+    if (!food) {
+      console.log('Error: food not found')
+      throw new AppError('Error: food not found', 401)
+    } else {
+      console.log(
+        `Food fetched: id: ${food.id}, name: ${food.name}`
+      )
+    }
+
+    console.log('Fetching image...')
+
+    // Handling case where avatar already exists
     if (food.image) {
       await diskStorage.deleteFile(food.image)
-      console.log("Previous image deleted")
+      console.log('Previous image deleted')
     } else {
-      console.log("No previous image found")
+      console.log('No previous image found')
     }
 
     const filename = await diskStorage.saveFile(imageFilename)
     food.image = filename
-      console.log("pass1")
+    console.log('pass1')
 
-    // Update user image in the database
-    await knex("foods").where({ id: user_id }).update(food)
-      console.log("pass2")
+    // Update user avatar in the database
+    await knex('foods').where({ id: foodId }).update(food)
+    console.log('pass2')
 
     // Sending success response
-    console.log("image updated successfully.")
+    console.log('Image updated successfully.')
     response.json({
-      message: "image updated successfully 2.",
+      message: 'Image updated successfully 2.',
       image: filename,
     })
   } catch (error) {
-    console.error("Error while updating image:", error)
+    console.error('Error while updating image:', error)
     response.status(error.statusCode || 500).json({ message: error.message })
   }
 }
 
-module.exports = UpdateFoodImage
+module.exports = UpdateImageController
